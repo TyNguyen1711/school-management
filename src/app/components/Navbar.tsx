49220a2901a0b3858c41390d45051440d181a6b7
22120419 <nguyenvanty147258@gmail.com>
2025-07-27 23:52:27 +0700

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="hidden lg:flex items-center gap-3 px-3 py-2 bg-transparent border border-gray-300 rounded-[100px]">
+        <Image src="/search.png" alt="Search" width={16} height={16} />
+        <input type="text" placeholder="Search..." className="outline-none" />
+      </div>
+      <div className="flex items-center gap-5 justify-end w-full">
+        <div className="bg-white p-2 rounded-full cursor-pointer">
+          <Image src="/message.png" alt="Messages" width={20} height={20} />
+        </div>
+        <div className="bg-white p-2 rounded-full cursor-pointer relative">
+          <Image
+            src="/announcement.png"
+            alt="Messages"
+            width={20}
+            height={20}
+          />
+          <div className="absolute -top-1 -right-1 bg-purple-500 text-white text-xs rounded-full px-1">
+            3
+          </div>
+        </div>
+        <div className="flex items-center gap-5">
+          <div className="flex flex-col items-end">
+            <div className="font-medium text-xs leading-3">John Does</div>
+            <div className="text-gray-500 text-[10px]">Teacher</div>
+          </div>
+          <Image
+            src="/avatar.png"
+            alt="avatar"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-3">
+        <Link
+          href="/Dashboard/teacher"
+          className="flex items-center justify-center lg:justify-start gap-3"
+        >
+          <Image src="/logo.png" alt="Logo" width={32} height={32} />
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] p-4 bg-[#F7F8FA] overflow-auto">
+        <Navbar />
+      </div>
+    </div>
+  );
+}
